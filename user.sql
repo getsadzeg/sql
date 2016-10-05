e42b7af341e5164001a97daeacd08a52324986b8
getsadzeg <getsadzeguram@gmail.com>
2016-10-06 00:44:57 +0400

    add first SQL file - playing with user table

---
+++ user.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE "USER"(
+   id INT PRIMARY KEY NOT NULL,
+   username VARCHAR(30) UNIQUE NOT NULL,
+   e_mail VARCHAR UNIQUE NOT NULL
+   );
+
+   INSERT INTO "USER" (id, username, e_mail) VALUES (1, 'guri', 'getsadzeguram@gmail.com');
+
+   SELECT id, username, e_mail FROM "USER" WHERE id=1;
