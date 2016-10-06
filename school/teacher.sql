5cdcba32960db52feb7d9cb8592e54da52b805cc
getsadzeg <getsadzeguram@gmail.com>
2016-10-06 17:59:46 +0400

    combine two sql files into one

--- school/teacher.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE TEACHER(
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    surname VARCHAR(30) NOT NULL,
-    birthdate date NOT NULL,
-    experience INT NOT NULL,
-    salary INT NOT NULL 
-    );
