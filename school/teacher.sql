461c6d33d8c0a74d4d4cbc1b52448d5d8cdad454
Guri Getsadze <getsadzeguram@gmail.com>
2016-10-06 17:50:01 +0400

    remove subject

--- school/teacher.sql
+++ school/teacher.sql
@@ -2,7 +2,6 @@ CREATE TABLE TEACHER(
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(30) NOT NULL,
     surname VARCHAR(30) NOT NULL,
-    subject VARCHAR(30) NOT NULL,
     birthdate date NOT NULL,
     experience INT NOT NULL,
     salary INT NOT NULL 
