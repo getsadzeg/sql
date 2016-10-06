5bd75a49efee850581df0431699ae46014085eca
getsadzeg <getsadzeguram@gmail.com>
2016-10-06 18:17:48 +0400

    add OFFICER, SUBJECT and referencing teacher to subject

--- school.sql
+++ school.sql
@@ -4,7 +4,8 @@ CREATE TABLE TEACHER(
     surname VARCHAR(30) NOT NULL,
     birthdate date NOT NULL,
     experience INT NOT NULL,
-    salary INT NOT NULL 
+    salary INT NOT NULL,
+    subject_id INT REFERENCES SUBJECT(id)
     );
 
 CREATE TABLE STUDENT(
@@ -14,4 +15,15 @@ CREATE TABLE STUDENT(
     birthdate date NOT NULL,
     class smallint NOT NULL,
     classname VARCHAR(1) NOT NULL
+    );
+
+CREATE TABLE OFFICER(
+    id serial PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL
+    );
+    
+CREATE TABLE SUBJECT(
+    id serial PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
     );
